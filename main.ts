b0e409e3f100f0bc6d3b186d64404a1f8889f611
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2023-11-07 14:16:59 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
+    Fentes += 1
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Go") {
+        debute = 2
+        basic.pause(3000)
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+        debute = 0
+    }
+})
+let debute = 0
+radio.setGroup(1)
+pins.setPull(DigitalPin.P0, PinPullMode.PullDown)
+let Fentes = 0
+let f1 = 0
+let f2 = 0
+debute = 0
+basic.showIcon(IconNames.No)
+basic.forever(function () {
+    while (debute == 2) {
+        f1 = Fentes
+        if (f1 > f2) {
+            radio.sendNumber(Fentes)
+        }
+        f2 = f1
+        basic.pause(100)
+    }
+})
